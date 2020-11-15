3e2e0c6ef218434ab9eb7e843789bacf17c5bc8c
Myles Pasetsky <mp3blue99@gmail.com>
2020-11-15 01:31:42 -0500

    Update key/hash typedefs, add key/value operators

--- containers/mp_hashmap.h
+++ containers/mp_hashmap.h
@@ -2,25 +2,30 @@
 #define __MP_HASHMAP__
 
 #include <stddef.h>
+#include <stdint.h>
 
-typedef long long hash_t;
+/*
+PROBLEM:
+    * The hashmap does not own the items added to it
+    * When using put(...), the key and the value should be copied and pointers to that 
+      should be stored in the linked list. When clearing, these pointers should be freed.
+    * This allows types like 'char *' to be safely added and freed 
+*/
+
+typedef uint64_t hash_t;
 
 // must be a power of 2
 #define __MP_HASHMAP_DEFAULT_BUCKETS__ (16)
-#define __MP_HASHMAP_DEFAULT_BUCKET_LOAD__ (0.75)
+#define __MP_HASHMAP_DEFAULT_BUCKET_LOAD__ (0.90)
 
-/*
-TODO: add necessary function pointers to add all needed operations
-to key_t and value_t (namely equals for key and value)
-*/
-typedef int key_t;
+typedef char * mkey_t;
 typedef int value_t;
 
 typedef struct __MP_HASHMAP_NODE__ 
 {
     struct __MP_HASHMAP_NODE__ * next;
     
-    key_t key;
+    mkey_t key;
     value_t value;
 
 } mp_hashmap_node;
@@ -50,13 +55,24 @@ typedef struct __MP_HASHMAP__
     size_t length;
 
     // Hash function
-    hash_t (*to_hash)(key_t);
+    hash_t (*to_hash)(const mkey_t *);
+
+    // Key Equals
+    int (*key_equals)(const mkey_t *, const mkey_t *);
+
+    // Value Equals
+    int (*value_equals)(const value_t *, const value_t *);
+
 } mp_hashmap;
 
 /* 
 Create a hashmap given a hashing function for key type.
 */
-mp_hashmap mphm_create(hash_t (*to_hash)(key_t));
+mp_hashmap mphm_create(
+    hash_t (*to_hash)(const mkey_t *),
+    int (*key_equals)(const mkey_t *, const mkey_t *),
+    int (*value_equals)(const value_t *, const value_t *)
+);
 
 // Returns if a hasmap is empty (has any elements)
 int mphm_is_empty(mp_hashmap *);
@@ -74,12 +90,12 @@ void mphm_free(mp_hashmap *);
 Get a pointer to a value for a given key.
 NULL if key not present
 */
-value_t * mphm_get(mp_hashmap *, key_t);
+value_t * mphm_get(mp_hashmap *, mkey_t);
 
 /*
 Check if a hashmap contains a key.
 */
-int mphm_contains(mp_hashmap *, key_t);
+int mphm_contains(mp_hashmap *, mkey_t);
 
 /*
 Check if a hashmap contains a value.
@@ -94,14 +110,14 @@ of key value pairs.
 
 Returns 1 if the key had not been present.
 */
-int mphm_put(mp_hashmap *, key_t, value_t);
+int mphm_put(mp_hashmap *, mkey_t, value_t);
 
 /*
 Removes a key, value pair based on its key.
 
 Returns 1 if the key was present and was removed
 */
-int mphm_remove(mp_hashmap *, key_t);
+int mphm_remove(mp_hashmap *, mkey_t);
 
 /*
 Removes key value pairs based on their values.
--- src/containers/mp_hashmap.c
+++ src/containers/mp_hashmap.c
@@ -2,7 +2,11 @@
 
 #include <stdlib.h>
 
-mp_hashmap mphm_create(hash_t (*to_hash)(key_t))
+mp_hashmap mphm_create(
+    hash_t (*to_hash)(const mkey_t *),
+    int (*key_equals)(const mkey_t *, const mkey_t *),
+    int (*value_equals)(const value_t *, const value_t *)
+)
 {
     mp_hashmap out = { 
         NULL, // bucket list
@@ -10,7 +14,9 @@ mp_hashmap mphm_create(hash_t (*to_hash)(key_t))
         0, // number of used buckets
         __MP_HASHMAP_DEFAULT_BUCKET_LOAD__, // bucket load (const)
         0, // number of elements
-        to_hash // hashing function
+        to_hash, // hashing function
+        key_equals, // key equals function
+        value_equals // value equals function
     };
 
     mp_hashmap_node ** new_buckets = calloc(out.total_buckets, sizeof(mp_hashmap_node *));
@@ -47,11 +53,11 @@ void mphm_free(mp_hashmap * map)
     map->total_buckets = 0;
 }
 
-static inline int is_power_of_2(unsigned int a){return (a!=0 && !(a & (a-1)));}
+static inline int is_power_of_2(hash_t a){return (a>0 && !(a & (a-1)));}
 
-static inline int get_index(mp_hashmap * map, key_t key)
+static inline int get_index(mp_hashmap * map, mkey_t key)
 {
-    hash_t hash = map->to_hash(key);
+    hash_t hash = map->to_hash(&key);
 
     // If somebody messed up the resizing on their own
     if (is_power_of_2(map->total_buckets))
@@ -60,7 +66,7 @@ static inline int get_index(mp_hashmap * map, key_t key)
         return hash % map->total_buckets;
 }
 
-static mp_hashmap_node * create_node(mp_hashmap_node * next, key_t key, value_t value)
+static mp_hashmap_node * create_node(mp_hashmap_node * next, mkey_t key, value_t value)
 {
     mp_hashmap_node * node = malloc(sizeof(mp_hashmap_node));
     node->key = key;
@@ -88,7 +94,7 @@ void mphm_resize(mp_hashmap * map, size_t buckets)
         while (top)
         {
             // index in the new map we're creating
-            const int o_index = map->to_hash(top->key) & (buckets-1);
+            const int o_index = map->to_hash(&top->key) & (buckets-1);
             used_buckets += (!new_buckets[o_index]);
             mp_hashmap_node * o_next = create_node(new_buckets[o_index], top->key, top->value);
             new_buckets[o_index] = o_next;
@@ -105,9 +111,9 @@ void mphm_resize(mp_hashmap * map, size_t buckets)
     map->used_buckets = used_buckets;
 }
 
-int mphm_put(mp_hashmap * map, key_t key, value_t value)
+int mphm_put(mp_hashmap * map, mkey_t key, value_t value)
 {
-    if ((float)(map->used_buckets/map->total_buckets) >= map->max_bucket_load)
+    while (map->used_buckets >= map->total_buckets * map->max_bucket_load)
         mphm_resize(map, map->total_buckets * 2);
 
     const int index = get_index(map, key);
@@ -116,7 +122,7 @@ int mphm_put(mp_hashmap * map, key_t key, value_t value)
     map->used_buckets += (!top);
     while (top)
     {
-        if (top->key == key)
+        if (map->key_equals(&top->key, &key))
         {
             top->value = value;
             return 0;
@@ -131,13 +137,13 @@ int mphm_put(mp_hashmap * map, key_t key, value_t value)
     return 1;
 }
 
-value_t * mphm_get(mp_hashmap * map, key_t key)
+value_t * mphm_get(mp_hashmap * map, mkey_t key)
 {
     const int index = get_index(map, key);
     mp_hashmap_node * top = map->buckets[index];
     while (top)
     {
-        if (top->key == key)
+        if (map->key_equals(&top->key, &key))
             return &top->value;
 
         top = top->next;
@@ -145,7 +151,7 @@ value_t * mphm_get(mp_hashmap * map, key_t key)
     return NULL;
 }
 
-int mphm_contains(mp_hashmap * map, key_t key) { return mphm_get(map, key) != NULL; }
+int mphm_contains(mp_hashmap * map, mkey_t key) { return mphm_get(map, key) != NULL; }
 
 int mphm_contains_value(mp_hashmap * map, value_t value)
 {
@@ -154,7 +160,7 @@ int mphm_contains_value(mp_hashmap * map, value_t value)
         mp_hashmap_node * top = map->buckets[i];
         while (top)
         {
-            if (top->value == value)
+            if (map->value_equals(&top->value, &value))
                 return 1;
 
             top = top->next;
@@ -163,7 +169,7 @@ int mphm_contains_value(mp_hashmap * map, value_t value)
     return 0;
 }
 
-int mphm_remove(mp_hashmap * map, key_t key)
+int mphm_remove(mp_hashmap * map, mkey_t key)
 {
     const int index = get_index(map, key);
     mp_hashmap_node * top = map->buckets[index];
@@ -172,7 +178,7 @@ int mphm_remove(mp_hashmap * map, key_t key)
     {
         mp_hashmap_node * next = top->next;
 
-        if (top->key == key)
+        if (map->key_equals(&top->key, &key))
         {
             // if we're NOT removing the first node
             if (before)
@@ -212,7 +218,7 @@ int mphm_remove_value(mp_hashmap * map, value_t value)
         {
             mp_hashmap_node * next = top->next;
 
-            if (top->value == value)
+            if (map->value_equals(&top->value, &value))
             {
                 if (before)
                     before->next = next;
